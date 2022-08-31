3a45636a74902b4a84e5903a93d6907efa1f29e4
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-08-31 16:21:42 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 const colorPallete = document.getElementById("color-palette");
 
 function addDivColorPalette() {
-    for (let index = 0; index < 4; index += 1) {
-        let newDiv = document.createElement("div");
-        newDiv.className = "color";
-        colorPallete.appendChild(newDiv);
-    }
+  for (let index = 0; index < 4; index += 1) {
+    const newDiv = document.createElement("div");
+    newDiv.className = "color";
+    colorPallete.appendChild(newDiv);
+  }
 }
 
 addDivColorPalette();
 
 function addColorInDivsPalette() {
-    const div = document.getElementsByClassName("color");
-    const colors = ["black", "blue", "aqua", "springgreen"];
-    for (let index = 0; index < div.length; index += 1) {
-        div[index].style.backgroundColor = colors[index];
-    }
+  const div = document.getElementsByClassName("color");
+  const colors = ["black", "blue", "aqua", "springgreen"];
+  for (let index = 0; index < div.length; index += 1) {
+    div[index].style.backgroundColor = colors[index];
+  }
 }
 
 addColorInDivsPalette();
+
+function createButton() {
+  const button = document.createElement("button");
+  button.id = "button-random-color";
+  button.innerText = "Cores aleatórias"
+  colorPallete.appendChild(button);
+}
+
+createButton();
+
+function randomColorGenerator() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
+
+function randomColorPalette() {
+  const divColors = document.getElementsByClassName("color");
+  for (let index = 1; index < divColors.length; index += 1) {
+    divColors[index].style.backgroundColor = randomColorGenerator();
+  }
+}
+
+function randomColorButton() {
+  const button = document.getElementById("button-random-color");
+  button.addEventListener("click", function(event) {
+    event.target = randomColorPalette();
+  });
+}
+
+randomColorButton();
